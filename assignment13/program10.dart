230494a1bf7b0df381dcf1e3048e7ac3ce8269cc
sanketsawant855 <sawantsanket855@gmail.com>
2024-09-15 01:08:01 +0530

    10

--- assignment13/program10.dart
+++ assignment13/program10.dart
@@ -1,1 +1,45 @@
-null
+import "dart:io";
+
+int powsum(int num1){
+  int sum=0;
+  while(num1>0){
+    int digit=num1%10;
+    sum+=(digit*digit);
+    num1~/=10;
+  }
+  return sum;
+
+}
+bool happy(int num){
+  while(true){
+    if(powsum(num)==1){
+    return true;
+  }else if(powsum(num)<10&&powsum(num)>1){
+    return false;
+  }else{
+    num=powsum(num);
+  }
+  }
+  
+}
+
+void main(){
+  stdout.write("enter number of rows : ");
+  int row=int.parse(stdin.readLineSync()!);
+  int num=1;
+  for(int i=1;i<=row;i++){
+    for(int j=1;j<=row;j++){
+      while(true){
+        if(happy(num)){
+        stdout.write("$num ");
+        num++;
+        break;
+      }else{
+        num++;
+      }
+      }
+      
+    }
+    print("");
+  }
+}
