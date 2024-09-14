8edb8df63a497d33aa9728ec22b0d3320a9ba9cf
sanketsawant855 <sawantsanket855@gmail.com>
2024-09-15 01:24:35 +0530

    6

--- assignment13/program6.dart
+++ assignment13/program6.dart
@@ -1,1 +1,27 @@
-null
+import "dart:io";
+
+void main(){
+  stdout.write("enter number of rows : ");
+  int row=int.parse(stdin.readLineSync()!);
+  int num=12;
+  int temp=20;
+  for(int i=1;i<=row;i++){
+    for(int j=1;j<=row;j++){
+      while(true){
+        if(num%temp==0){
+          stdout.write("$num ");
+          num++;
+          temp*=row-1;
+          break;
+        }
+        if(num%6==0){
+          stdout.write("$num ");
+          num++;
+          break;
+        }
+        num++;
+      }
+    }
+    print("");
+  }
+}
